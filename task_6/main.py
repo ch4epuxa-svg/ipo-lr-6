edc66f9ca7955b446e50fabcf9c0ab4f33254ec8
rimmachepuxa <ch4epuxa@gmail.com>
2025-12-04 19:46:18 +0300

    Update main.py

--- task_6/main.py
+++ task_6/main.py
@@ -1,14 +1,14 @@
 # вариант 4
-from random import randint#импортируем функцию для случайног очисла
+from random import randint#импортируем функцию для случайного числа
 from itertools import combinations#исспортируем функцию для нахождения уникальных пар
 
 random_number = [tuple(randint(-10, 10) for i in range(4)) for j in range(20)]#создаем список изх 20 значений по 4 числа
-print("Исходный список (20 элементов по 4 числа):")#выводим текст
+print("Исходный список (20 элементов по 4 числа): ")#выводим текст
 for i, row in enumerate(random_number, 1):#проходимся по списку
     print(f"{i}: {row}")#выводим по строчно
 
-unique_combinations = list({tuple(sorted(row)) for row in random_number})#находим все уникальные  комбинации
-print("Уникальные комбинации (порядок не учитывается):")#выводим текст
+unique_combinations = list({tuple(sorted(row)) for row in random_number})#находим все уникальные комбинации
+print("Уникальные комбинации, не учитывая порядок: ")#выводим текст
 for combo in unique_combinations:#проходимя по уникальным комбинациям
     print(combo)#вывод их
 
@@ -23,4 +23,4 @@ for a, b in pairs:#проходимя по этим парама
         count += 1#плюс один к счетчику
 
 print(f"Количество пар, сумма которых меньше {n}: {count}")#выводим сколько пар меньеше чем n
-#
+
